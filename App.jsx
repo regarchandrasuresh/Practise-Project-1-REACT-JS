5054ca806e02ec57e5c7814f477312690db1d07e
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-08-16 09:39:33 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import ProductGroup from './ProductGroup';
+
+
+
+function App() {
+    return (
+      <>
+      <h1 style={{textAlign:"center"}}>SMART - PHONE</h1>
+     
+    <ProductGroup/>
+ </>
+  )
+}
+
+export default App;
